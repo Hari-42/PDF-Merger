16082b3200e87a8bc6bcae605641295ae07e4cf2
Hari-42 <harisaranmohanathas@hotmail.com>
2025-02-13 19:20:58 +0100

    displaying the PDFs in a list

---
+++ script.js
@@ -0,0 +1,16 @@
+const fileInput = document.getElementById("pdfInput");
+const dropZone = document.getElementById("dropZone");
+const fileList = document.getElementById("fileList");
+
+function updateList(files) {
+    fileList.innerHTML = [...files].map(file => `<li class="bg-white p-2 mt-2 rounded-lg shadow text-gray-700">${file.name}</li>`).join("");
+}
+
+fileInput.onchange = (e) => updateList(e.target.files);
+
+dropZone.ondragover = (e) => e.preventDefault();
+dropZone.ondrop = (e) => {
+    e.preventDefault();
+    fileInput.files = e.dataTransfer.files;
+    updateList(e.dataTransfer.files);
+};
