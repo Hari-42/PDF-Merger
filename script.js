e13a66f247d5af4f1171c82142ee0874e65f8b2a
Hari-42 <harisaranmohanathas@hotmail.com>
2025-02-28 23:18:05 +0100

    put the pdf files in a array

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const fileInput = document.getElementById("pdfInput");
 const dropZone = document.getElementById("dropZone");
 const fileList = document.getElementById("fileList");
 
+let pdfFiles = [];
+
 function updateList(files) {
     fileList.innerHTML = [...files].map(file => `<li class="bg-white p-2 mt-2 rounded-lg shadow text-gray-700">${file.name}</li>`).join("");
+    pdfFiles = [...files];
 }
+
 
 fileInput.onchange = (e) => updateList(e.target.files);
 
@@ -12,5 +16,5 @@ dropZone.ondragover = (e) => e.preventDefault();
 dropZone.ondrop = (e) => {
     e.preventDefault();
     fileInput.files = e.dataTransfer.files;
-    updateList(e.dataTransfer.files);
+    updateList(e.dataTransfer.files)
 };
